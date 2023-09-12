71d18a81a7c3a2ad1d1410144026e286f20e211b
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-09-12 14:38:17 +0200

    added schema to seed script

--- lib/mix/seed.ex
+++ lib/mix/seed.ex
@@ -13,6 +13,10 @@ defmodule Mix.Tasks.Seed do
     IO.puts("Dropping tables")
     Postgrex.query!(DB, "DROP TABLE IF EXISTS fruits", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS users", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS ordered_pizzas", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS orders", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS pizzas", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS ingredients", [], pool: DBConnection.ConnectionPool)
   end
 
   defp create_tables() do
@@ -20,25 +24,36 @@ defmodule Mix.Tasks.Seed do
 
     Postgrex.query!(
       DB,
-      "Create TABLE fruits (id SERIAL, name VARCHAR(255) NOT NULL, tastiness INTEGER NOT NULL)",
+      "CREATE TABLE ingredients (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, price INTEGER NOT NULL)",
       [],
       pool: DBConnection.ConnectionPool
     )
+    Postgrex.query!(
+      DB,
+      "CREATE TABLE pizzas (id SERIAL PRIMARY KEY, ingredients INTEGER NOT NULL, name VARCHAR(255) NOT NULL, price INTEGER NOT NULL)",
+      [],
+      pool: DBConnection.ConnectionPool
+    )
+    Postgrex.query!(
+      DB,
+      "CREATE TABLE orders (id SERIAL PRIMARY KEY, status INTEGER NOT NULL, ordered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, customer VARCHAR(255) NOT NULL, price INTEGER NOT NULL)",
+      [],
+      pool: DBConnection.ConnectionPool
+    )
+    Postgrex.query!(
+      DB,
+      "CREATE TABLE ordered_pizzas (id SERIAL PRIMARY KEY, order_id INTEGER NOT NULL, ingredients INTEGER NOT NULL, template INTEGER NOT NULL, FOREIGN KEY (order_id) REFERENCES orders (id), FOREIGN KEY (template) REFERENCES pizzas (id))",
+      [],
+      pool: DBConnection.ConnectionPool
+    )
+
   end
 
   defp seed_data() do
     IO.puts("Seeding data")
 
-    Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Apple", 5],
-      pool: DBConnection.ConnectionPool
-    )
-
-    Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Pear", 4],
-      pool: DBConnection.ConnectionPool
-    )
-
-    Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Banana", 7],
-      pool: DBConnection.ConnectionPool
-    )
+    # Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Apple", 5],
+    #   pool: DBConnection.ConnectionPool
+    # )
   end
 end
