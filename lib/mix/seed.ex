61227799b6bfc0b46961f0befca13ad1b7ce6a5d
ntijoh-christian-thorp <christian.thorp@elev.ga.ntig.se>
2023-09-12 14:51:28 +0200

    added seed data in ingredients

--- lib/mix/seed.ex
+++ lib/mix/seed.ex
@@ -52,8 +52,64 @@ defmodule Mix.Tasks.Seed do
   defp seed_data() do
     IO.puts("Seeding data")
 
-    # Postgrex.query!(DB, "INSERT INTO fruits(name, tastiness) VALUES($1, $2)", ["Apple", 5],
-    #   pool: DBConnection.ConnectionPool
-    # )
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Skinka", 999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Oliver", 1999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Tomatsås", 499],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Mozzarella", 499],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Basilika", 499],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Svamp", 1499],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Kronärtskocka", 999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Parmesan", 2999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Pecorino", 999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Gorgonzola", 1499],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Paprika", 999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Aubergine", 999],
+       pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Zucchini", 999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Salami", 1999],
+       pool: DBConnection.ConnectionPool
+     )
+
+    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Chili", 999],
+       pool: DBConnection.ConnectionPool
+    )
   end
 end
