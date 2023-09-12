38c9bd5b87b9d9e935242c7232f37e778fbf1378
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-09-12 15:28:58 +0200

    added seed data for pizzas

--- lib/mix/seed.ex
+++ lib/mix/seed.ex
@@ -1,3 +1,5 @@
+import Bitwise
+
 defmodule Mix.Tasks.Seed do
   use Mix.Task
 
@@ -13,7 +15,11 @@ defmodule Mix.Tasks.Seed do
     IO.puts("Dropping tables")
     Postgrex.query!(DB, "DROP TABLE IF EXISTS fruits", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS users", [], pool: DBConnection.ConnectionPool)
-    Postgrex.query!(DB, "DROP TABLE IF EXISTS ordered_pizzas", [], pool: DBConnection.ConnectionPool)
+
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS ordered_pizzas", [],
+      pool: DBConnection.ConnectionPool
+    )
+
     Postgrex.query!(DB, "DROP TABLE IF EXISTS orders", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS pizzas", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS ingredients", [], pool: DBConnection.ConnectionPool)
@@ -28,88 +34,192 @@ defmodule Mix.Tasks.Seed do
       [],
       pool: DBConnection.ConnectionPool
     )
+
     Postgrex.query!(
       DB,
       "CREATE TABLE pizzas (id SERIAL PRIMARY KEY, ingredients INTEGER NOT NULL, name VARCHAR(255) NOT NULL, price INTEGER NOT NULL)",
       [],
       pool: DBConnection.ConnectionPool
     )
+
     Postgrex.query!(
       DB,
       "CREATE TABLE orders (id SERIAL PRIMARY KEY, status INTEGER NOT NULL, ordered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, customer VARCHAR(255) NOT NULL, price INTEGER NOT NULL)",
       [],
       pool: DBConnection.ConnectionPool
     )
+
     Postgrex.query!(
       DB,
       "CREATE TABLE ordered_pizzas (id SERIAL PRIMARY KEY, order_id INTEGER NOT NULL, ingredients INTEGER NOT NULL, template INTEGER NOT NULL, FOREIGN KEY (order_id) REFERENCES orders (id), FOREIGN KEY (template) REFERENCES pizzas (id))",
       [],
       pool: DBConnection.ConnectionPool
     )
-
   end
 
   defp seed_data() do
     IO.puts("Seeding data")
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Skinka", 999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Oliver", 1999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Tomatsås", 499],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
-    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Mozzarella", 499],
-       pool: DBConnection.ConnectionPool
-     )
+    Postgrex.query!(
+      DB,
+      "INSERT INTO ingredients(name, price) VALUES($1, $2)",
+      ["Mozzarella", 499],
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Basilika", 499],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Svamp", 1499],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
-    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Kronärtskocka", 999],
-       pool: DBConnection.ConnectionPool
-     )
+    Postgrex.query!(
+      DB,
+      "INSERT INTO ingredients(name, price) VALUES($1, $2)",
+      ["Kronärtskocka", 999],
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Parmesan", 2999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Pecorino", 999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
-    Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Gorgonzola", 1499],
-       pool: DBConnection.ConnectionPool
-     )
+    Postgrex.query!(
+      DB,
+      "INSERT INTO ingredients(name, price) VALUES($1, $2)",
+      ["Gorgonzola", 1499],
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Paprika", 999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Aubergine", 999],
-       pool: DBConnection.ConnectionPool
+      pool: DBConnection.ConnectionPool
     )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Zucchini", 999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Salami", 1999],
-       pool: DBConnection.ConnectionPool
-     )
+      pool: DBConnection.ConnectionPool
+    )
 
     Postgrex.query!(DB, "INSERT INTO ingredients(name, price) VALUES($1, $2)", ["Chili", 999],
-       pool: DBConnection.ConnectionPool
+      pool: DBConnection.ConnectionPool
     )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [parse_to_bitfield(["Tomatsås", "Mozzarella", "Basilika"]), "Margherita", 10999],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [parse_to_bitfield(["Tomatsås"]), "Marinara", 10999],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Skinka", "Svamp"]),
+        "Prosciutto e funghi",
+        12999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Skinka", "Svamp", "Kronärtskocka", "Oliver"]),
+        "Quattro stagioni",
+        13999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Skinka", "Svamp", "Kronärtskocka"]),
+        "Capricciosa",
+        12999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Parmesan", "Pecorino", "Gorgonzola"]),
+        "Quattro formaggi",
+        15999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Paprika", "Aubergine", "Zucchini"]),
+        "Ortolana",
+        11999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+
+    Postgrex.query!(
+      DB,
+      "INSERT INTO pizzas(ingredients, name, price) VALUES($1, $2, $3)",
+      [
+        parse_to_bitfield(["Tomatsås", "Mozzarella", "Salami", "Paprika", "Chili"]),
+        "Diavola",
+        14999
+      ],
+      pool: DBConnection.ConnectionPool
+    )
+  end
+
+  defp get_ingredient_ids() do
+    %{rows: ingredient_ids} =
+      Postgrex.query!(DB, "SELECT id, name FROM ingredients ORDER BY id", [])
+
+    ingredient_ids |> Enum.reduce(%{}, fn [id, name], acc -> acc |> Map.put(name, id) end)
+  end
+
+  defp parse_to_bitfield(ingredients) do
+    Enum.reduce(ingredients, 0, fn x, acc ->
+      acc ||| 1 <<< Map.get(get_ingredient_ids(), x)
+    end)
   end
 end
