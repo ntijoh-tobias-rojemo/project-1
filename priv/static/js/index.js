e62f37665307f52c15f65fa939daee216903d30d
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-09-13 13:57:51 +0200

    cleaned up js

--- priv/static/js/index.js
+++ priv/static/js/index.js
@@ -1,16 +1,13 @@
 document.querySelectorAll("main > div").forEach((pizza) => {
   const data = `${pizza.dataset.id}§${pizza.dataset.name}§${pizza.dataset.ingredients}§${pizza.dataset.price}`;
-  pizza.querySelector(".customize").addEventListener("click", () => {
+  const add = () => {
     const order = localStorage.getItem("order")?.split("§§") || [];
     order.push(data);
     localStorage.setItem("order", order.join("§§"));
-
+  };
+  pizza.querySelector(".customize").addEventListener("click", () => {
+    add();
     location.assign("/cart");
   });
-
-  pizza.querySelector(".buy").addEventListener("click", () => {
-    const order = localStorage.getItem("order")?.split("§§") || [];
-    order.push(data);
-    localStorage.setItem("order", order.join("§§"));
-  });
+  pizza.querySelector(".buy").addEventListener("click", add);
 });
