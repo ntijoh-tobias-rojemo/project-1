243d3a5697aa9e6af007d9af49c4340cd23723aa
ntijoh-mathias-malmqvist <143790852+ntijoh-mathias-malmqvist@users.noreply.github.com>
2023-09-14 14:47:15 +0200

    Added animation to cart

--- priv/static/js/index.js
+++ priv/static/js/index.js
@@ -1,3 +1,5 @@
+const cart = document.querySelector(".basket > img");
+
 document.querySelectorAll("main > div").forEach((pizza) => {
   const data = `${pizza.dataset.id}§${pizza.dataset.name}§${pizza.dataset.ingredients}§${pizza.dataset.price}`;
   pizza.querySelector(".customize").addEventListener("click", () => {
@@ -10,5 +12,20 @@ document.querySelectorAll("main > div").forEach((pizza) => {
     const order = localStorage.getItem("order")?.split("§§") || [];
     order.push(data);
     localStorage.setItem("order", order.join("§§"));
+    cart.animate(
+      [
+        { transform: "translate(0, 0) rotate(0deg)" },
+        { transform: "translate(5px, -15px) rotate(5deg)" },
+        { transform: "translate(0, 0) rotate(0eg)" },
+        { transform: "translate(-5px, -15px) rotate(-5deg)" },
+        { transform: "translate(0, 0) rotate(0deg)" },
+      ],
+      {
+        duration: 125,
+        iterations: 2,
+        easing: "ease-in-out",
+        fill: "forwards",
+      }
+    );
   });
 });
