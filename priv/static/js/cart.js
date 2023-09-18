d6b74ee703276c28cff4fa36bcb606d350d42790
Tobias <tobias.rojemo@elev.ga.ntig.se>
2023-09-18 12:31:49 +0200

    replaced pizza edit system

--- priv/static/js/cart.js
+++ priv/static/js/cart.js
@@ -39,8 +39,24 @@ const order =
 const main = document.querySelector("main");
 
 order.forEach((pizza, i) => {
-  const elem = document.createElement("div");
-  elem.id = i;
+  const div = document.createElement("div");
+  div.id = i;
+
+  fillPizza(div, pizza, i);
+
+  main.appendChild(div);
+});
+
+const match = location.href.match(/#(\d+)/);
+
+if (match) {
+  setTimeout(() => {
+    document.getElementById(match[1]).scrollIntoView({ behavior: "smooth" });
+  }, 100);
+}
+
+function fillPizza(div, pizza, i) {
+  div.textContent = "";
 
   const img = document.createElement("img");
   img.src = `/img/${pizza.id}.svg`;
@@ -55,28 +71,60 @@ order.forEach((pizza, i) => {
   ingredientList.classList.add("ingredients");
 
   ingredients
-    .filter(
-      (ingredient) =>
-        (pizzas[pizza.id - 1].ingredients & (1 << ingredient.id)) > 0
-    )
+    .filter((ingredient) => (pizza.ingredients & (1 << ingredient.id)) > 0)
     .forEach((ingredient) => {
-      const p = document.createElement("p");
-      p.innerHTML = ingredient.name;
-      ingredientList.appendChild(p);
+      const name = document.createElement("span");
+      name.innerHTML = ingredient.name;
+      ingredientList.appendChild(name);
+
+      const remove = document.createElement("button");
+      remove.innerHTML = "Ta bort";
+      ingredientList.appendChild(remove);
+      remove.addEventListener("click", () => {
+        order[i].ingredients &= ~(1 << ingredient.id);
+        localStorage.setItem(
+          "order",
+          order
+            .map((x) => `${x.id}§${x.name}§${x.ingredients}§${x.price}`)
+            .join("§§")
+        );
+        span.innerHTML = `${calcPrice(pizza)}:-`;
+
+        fillPizza(div, pizza, i);
+      });
+
+      const br = document.createElement("br");
+      ingredientList.appendChild(br);
     });
 
-  const options = document.createElement("div");
-  options.classList.add("options");
-  ingredients.forEach((ingredient) => {
-    appendBox(
-      options,
-      i,
-      ingredient,
-      pizza.ingredients & (1 << ingredient.id),
-      (pizzas[pizza.id - 1].ingredients & (1 << ingredient.id)) == 0,
-      pizza,
-      span
+  const select = document.createElement("select");
+  select.classList.add("options");
+  const noOption = document.createElement("option");
+  noOption.value = 0;
+  noOption.innerHTML = "Lägg till en ingrediens";
+  select.appendChild(noOption);
+  ingredients
+    .filter((ingredient) => (pizza.ingredients & (1 << ingredient.id)) == 0)
+    .forEach((ingredient) => {
+      appendOption(
+        select,
+        ingredient,
+        (pizzas[pizza.id - 1].ingredients & (1 << ingredient.id)) == 0
+      );
+    });
+
+  select.addEventListener("input", (_) => {
+    const ingredientId = select.value;
+    order[i].ingredients |= 1 << ingredientId;
+    localStorage.setItem(
+      "order",
+      order
+        .map((x) => `${x.id}§${x.name}§${x.ingredients}§${x.price}`)
+        .join("§§")
     );
+    span.innerHTML = `${calcPrice(pizza)}:-`;
+
+    fillPizza(div, pizza, i);
   });
 
   const remove = document.createElement("button");
@@ -95,11 +143,11 @@ order.forEach((pizza, i) => {
     location.reload();
   });
 
-  const remove_all = document.createElement("button");
-  remove_all.classList.add("clear");
-  remove_all.innerHTML = "Clear Order";
-  remove_all.addEventListener("click", () => {
-  localStorage.clear("order");
+  const removeAll = document.createElement("button");
+  removeAll.classList.add("clear");
+  removeAll.innerHTML = "Clear Order";
+  removeAll.addEventListener("click", () => {
+    localStorage.clear("order");
     location.reload();
   });
 
@@ -109,51 +157,26 @@ order.forEach((pizza, i) => {
   info.appendChild(h);
   info.appendChild(span);
   info.appendChild(ingredientList);
-  info.appendChild(options);
+  info.appendChild(select);
   info.appendChild(remove);
-  info.appendChild(remove_all)
+  info.appendChild(removeAll);
 
-  elem.appendChild(img);
-  elem.appendChild(info);
-  main.appendChild(elem);
-});
-
-const match = location.href.match(/#(\d+)/);
-
-if (match) {
-  setTimeout(() => {
-    document.getElementById(match[1]).scrollIntoView({ behavior: "smooth" });
-  }, 100);
+  div.appendChild(img);
+  div.appendChild(info);
 }
 
-function appendBox(list, i, ingredient, enabled, showPrice, pizza, priceElem) {
-  const box = document.createElement("input");
-  box.type = "checkbox";
-  box.id = `${i}-${ingredient.id}`;
-  box.checked = enabled;
-  const label = document.createElement("label");
-  label.for = `${i}-${ingredient.id}`;
+function appendOption(list, ingredient, showPrice) {
+  const option = document.createElement("option");
+  option.value = ingredient.id;
   if (ingredient.name == "Familjepizza") {
-    label.innerHTML = `${ingredient.name} - x2`;
+    option.innerHTML = `${ingredient.name} - x2`;
   } else if (ingredient.price == 0 || !showPrice) {
-    label.innerHTML = `${ingredient.name}`;
+    option.innerHTML = `${ingredient.name}`;
   } else {
-    label.innerHTML = `${ingredient.name} - ${ingredient.price / 100}:-`;
+    option.innerHTML = `${ingredient.name} - ${ingredient.price / 100}:-`;
   }
 
-  box.addEventListener("click", () => {
-    order[i].ingredients ^= 1 << ingredient.id;
-    localStorage.setItem(
-      "order",
-      order
-        .map((x) => `${x.id}§${x.name}§${x.ingredients}§${x.price}`)
-        .join("§§")
-    );
-    priceElem.innerHTML = `${calcPrice(pizza)}:-`;
-  });
-
-  list.appendChild(box);
-  list.appendChild(label);
+  list.appendChild(option);
 }
 
 function calcPrice(pizza) {
